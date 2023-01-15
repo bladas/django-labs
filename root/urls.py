a9e6cfede84284c9d0ad6bc80758ab8240530a72
bladas <daskevichvladislav25@gmail.com>
2023-01-15 23:23:23 +0200

    - rename module

--- root/urls.py
+++ root/urls.py
@@ -1,4 +1,4 @@
-"""lab1 URL Configuration
+"""root URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
