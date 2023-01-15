2979a8edc92f618a3f4d69ed05288f75962e0a48
bladas <daskevichvladislav25@gmail.com>
2023-01-15 23:19:43 +0200

    lab 2

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home.html', context=None)
